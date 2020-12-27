245a603d233b67cc6cab221e258d80d392d04483
dm1tro69 <dimolg22@gmail.com>
2020-12-27 20:54:34 +0200

    add bodyParser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send(`
@@ -16,18 +18,13 @@ app.get('/', (req, res) => {
     `)
 });
 
-app.post('/', (req, res) => {
-    req.on('data', (data) => {
-        const parsed = data.toString('utf8').split('&')
-        const formData = {};
-        for (let pair of parsed){
-            const [key, value] = pair.split('=')
-            formData[key] = value
-        }
-        console.log(formData)
-    })
+
+
+app.post('/',(req, res) => {
+     console.log(req.body)
     res.send('Account Created')
-})
+});
+
 
 app.listen(3000, () => {
     console.log('Listening')
