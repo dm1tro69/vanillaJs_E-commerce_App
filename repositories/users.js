04098bc62b57f1f4b62b4e3eb1ff92676be4d137
dm1tro69 <dimolg22@gmail.com>
2020-12-29 22:14:06 +0200

    add authentication

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,9 @@
 
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt)
 
 class UsersRepository {
     constructor(filename) {
@@ -26,11 +29,25 @@ class UsersRepository {
 
     async create(attrs) {
         attrs.id = this.randomId();
+        const salt = crypto.randomBytes(8).toString('hex');
+        const hashed = await scrypt(attrs.password, salt, 64)
 
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${hashed.toString('hex')}.${salt}`
+        }
+        records.push(record);
 
         await this.writeAll(records);
+        return record
+    }
+
+    async comparePasswords(saved, supplied) {
+
+        const [hashed, salt] = saved.split('.')
+        const hashedSupplied = await scrypt(supplied, salt, 64)
+        return hashed === hashedSupplied.toString('hex')
     }
 
     async writeAll(records) {
